499fda0ea4a3d6970884eb5bca6c0a367ed58ccc
Ubuntu <ubuntu@ip-172-31-11-192.us-east-2.compute.internal>
2022-02-09 03:56:53 +0000

    adding form for input

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import twit
 import geni
 from flask import render_template
+from mlt_data import *
 
     
 app = flask.Flask(__name__)
@@ -14,8 +15,16 @@ app = flask.Flask(__name__)
 def main():
     song = geni.get_song_data()
     tweet = twit.get_tweet() 
-    
+    data = MLT_Data("fellow_data.json")
+
     return render_template('index.html', song_data = song, twitter_data = tweet)
+    
+    
+@app.route('/form', methods = ['POST', 'GET'])
+def form():
+    return render_template('form.html')
+    
+    
     
 app.run(
     port = int(os.getenv('PORT')),
