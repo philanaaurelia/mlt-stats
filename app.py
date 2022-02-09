4749d3c9cc1a3578ce28e12ac1826ed292bef659
Ubuntu <ubuntu@ip-172-31-11-192.us-east-2.compute.internal>
2022-02-09 16:58:08 +0000

    removing unecessary files and code

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import os
 import random
 import requests
 import json
-import twit
-import geni
 from flask import render_template
 from flask import request, redirect
 from mlt_data import *
@@ -14,14 +12,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-    
-    song = geni.get_song_data()
-    tweet = twit.get_tweet() 
     # data = MLT_Data("fellow_data.json")
     data = MLT_Data("test.json")
     fellow = data.get_fellow_data("philanab@gmail.com")
 
-    return render_template('index.html', song_data = song, twitter_data = tweet, fellow_data = fellow)
+    return render_template('index.html', fellow_data = fellow)
     
     
 @app.route('/form', methods = ['POST', 'GET'])
