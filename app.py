6112375cb6a52a21b92004993185ccf69d94c2eb
Ubuntu <ubuntu@ip-172-31-11-192.us-east-2.compute.internal>
2022-02-11 01:33:15 +0000

    trial for heroku https

--- app.py
+++ app.py
@@ -2,23 +2,36 @@ import flask
 import os 
 import random
 import json
-from flask_googlelogin import GoogleLogin
+
 from flask import render_template
 from flask import session, redirect, url_for
 from mlt_data import *
-from flask_login import (UserMixin, login_required, login_user, logout_user,
+from flask_login import (LoginManager, UserMixin, login_required, login_user, logout_user,
                          current_user)
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.consumer import oauth_authorized
     
 app = flask.Flask(__name__)
-app.config.update(
-    SECRET_KEY='AIzaSyAuQo0MS-TgejKC75M9uGTeU0WXnyxeOww',
-    GOOGLE_LOGIN_CLIENT_ID="415070769795-0a1chfviiumsdsv7ov2fcaqdlotut530.apps.googleusercontent.com",
-    GOOGLE_LOGIN_CLIENT_SECRET='zT2-uqTe1wqdJGWUs7Y8LZvX',
-    GOOGLE_LOGIN_REDIRECT_URI='https://mlt-stats.herokuapp.com/oauth2callback')
-googlelogin = GoogleLogin(app)
+
+SECRET_KEY='AIzaSyAuQo0MS-TgejKC75M9uGTeU0WXnyxeOww'
+GOOGLE_LOGIN_CLIENT_ID="415070769795-0a1chfviiumsdsv7ov2fcaqdlotut530.apps.googleusercontent.com"
+GOOGLE_LOGIN_CLIENT_SECRET='zT2-uqTe1wqdJGWUs7Y8LZvX'
+GOOGLE_LOGIN_REDIRECT_URI='https://mlt-stats.herokuapp.com/oauth2callback'
+
+app.config['SECRET_KEY'] = SECRET_KEY
+goog_blueprint = make_google_blueprint(
+    client_id= GOOGLE_LOGIN_CLIENT_ID,
+    client_secret= GOOGLE_LOGIN_CLIENT_SECRET,
+    reprompt_consent=True
+)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'google.login'
+
+app.register_blueprint(goog_blueprint)
 
 @app.route('/home')
-@login_required
 def home():
     # data = MLT_Data("fellow_data.json")
     data = MLT_Data("test.json")
@@ -28,25 +41,42 @@ def home():
     
     
 @app.route('/oauth2callback')
-@googlelogin.oauth2callback
-def callback(token, userinfo, **params):
-    user = User.filter_by(google_id=userinfo['id']).first()
-    if user:
-        user.name = userinfo['name']
-        user.avatar = userinfo['picture']
+@oauth_authorized.connect_via(goog_blueprint)
+def google_logged_in(blueprint, token):
+    resp = blueprint.session.get('/oauth2/v2/userinfo')
+    user_info = resp.json()
+    print(user_info)
+    user_id = str(user_info['id'])
+    oauth = OAuth.query.filter_by(provider=blueprint.name,
+                                  provider_user_id=user_id).first()
+    if not oauth:
+        oauth = OAuth(provider=blueprint.name,
+                      provider_user_id=user_id,
+                      token=token)
     else:
-        user = User(google_id=userinfo['id'],
-                    name=userinfo['name'],
-                    avatar=userinfo['picture'])
-    db.session.add(user)
-    db.session.flush()
-    login_user(user)
-    return redirect(url_for('home'))
+        oauth.token = token
+        db.session.add(oauth)
+        db.session.commit()
+        login_user(oauth.user)
+    if not oauth.user:
+        user = User(email=user_info["email"],
+                    name=user_info["name"])
+        oauth.user = user
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        login_user(user)
+
+    return False
     
+    # return redirect(url_for('home'))
     
 @app.route('/')
 def main():
-    return render_template('login.html', google_url = googlelogin.login_url(approval_prompt='force'))
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+    return render_template('login.html', google_url = "/google", google_data = google_data)
     
 
     
