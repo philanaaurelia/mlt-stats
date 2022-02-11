9cb63b3fd3e3cc99fce730aac462f8da6302b2e4
Ubuntu <ubuntu@ip-172-31-11-192.us-east-2.compute.internal>
2022-02-11 00:06:37 +0000

    fix redirect

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import flask
 import os 
 import random
-import requests
 import json
 from flask_googlelogin import GoogleLogin
 from flask import render_template
-from flask import request, redirect, url_for
+from flask import session, redirect, url_for
 from mlt_data import *
 from flask_login import (UserMixin, login_required, login_user, logout_user,
                          current_user)
@@ -15,7 +14,7 @@ app.config.update(
     SECRET_KEY='AIzaSyAuQo0MS-TgejKC75M9uGTeU0WXnyxeOww',
     GOOGLE_LOGIN_CLIENT_ID="415070769795-0a1chfviiumsdsv7ov2fcaqdlotut530.apps.googleusercontent.com",
     GOOGLE_LOGIN_CLIENT_SECRET='zT2-uqTe1wqdJGWUs7Y8LZvX',
-    GOOGLE_LOGIN_REDIRECT_URI='https://a102ae043f294e90bb07ce6f68d9186f.vfs.cloud9.us-east-2.amazonaws.com/oauth2callback')
+    GOOGLE_LOGIN_REDIRECT_URI='https://mlt-stats.herokuapp.com/oauth2callback')
 googlelogin = GoogleLogin(app)
 
 @app.route('/home')
@@ -31,29 +30,30 @@ def home():
 @app.route('/oauth2callback')
 @googlelogin.oauth2callback
 def callback(token, userinfo, **params):
-    user = users[userinfo['id']] = User(userinfo)
+    user = User.filter_by(google_id=userinfo['id']).first()
+    if user:
+        user.name = userinfo['name']
+        user.avatar = userinfo['picture']
+    else:
+        user = User(google_id=userinfo['id'],
+                    name=userinfo['name'],
+                    avatar=userinfo['picture'])
+    db.session.add(user)
+    db.session.flush()
     login_user(user)
-    session['token'] = json.dumps(token)
-    session['extra'] = params.get('extra')
-    return redirect(params.get('next', url_for('/home')))
+    return redirect(url_for('form'))
     
     
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/')
 def main():
-    return render_template('login.html', google_url = googlelogin.login_url(params=dict(section='notifications', next=url_for('.callback'))))
+    return render_template('login.html', google_url = googlelogin.login_url(approval_prompt='force'))
     
 
     
 @app.route('/form', methods = ['POST', 'GET'])
 def form():
     return render_template('form.html')
-    
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-    yo = request.form['call-schedule']
-    print("The email address is '" + yo + "'")
-    return main()
-    
+
 app.run(
     port = int(os.getenv('PORT')),
     host = os.getenv('IP','0.0.0.0'),
